9ba65c1c3cf6c24e39af2aba1195adfd24021c8d
Honeypot <eric@thezombiecoder.com>
2023-02-16 17:22:54 -0800

    Provided a description for the following functions: navigationGenres,genrePopupForm

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import navigateGenres from './modules/navigateGenres.js'
 import genrePopupForm from './modules/genrePopupForm.js'
 
-
+// Allows users to navigate different genres and their associated movies
 navigateGenres()
+// Allows users to add a new movie and/or genre to the database
 genrePopupForm()
