33d74a16f817bb7f37fe3b14b321a70449f90ecf
Honeypot <eric@thezombiecoder.com>
2023-02-17 12:58:26 -0800

    imported the insertGenres module and invoked it

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+import insertGenres from './modules/insertGenres.js'
 import navigateGenres from './modules/navigateGenres.js'
 import genrePopupForm from './modules/genrePopupForm.js'
 
+
+// Populates drop down menus with movie genres
+insertGenres()
 // Allows users to navigate different genres and their associated movies
 navigateGenres()
 // Allows users to add a new movie and/or genre to the database
