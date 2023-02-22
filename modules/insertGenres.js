d534fd2218964cd01fdadaf18cc3cb99309b8711
Honeypot <eric@thezombiecoder.com>
2023-02-21 17:26:58 -0800

    Exported movies object, and removed export default syntax

--- modules/insertGenres.js
+++ modules/insertGenres.js
@@ -3,7 +3,7 @@ import {
   genreForm as popupGenreDropdown,
 } from './selectors.js'
 
-const movies = {
+export const movies = {
   comedy: ['17-again', 'bruce-almighty', 'the-proposal'],
   drama: ['good-will-hunting', 'lean-on-me', 'the-blind-side'],
   thriller: ['lucky-number-slevin', 'memory', 'the-code'],
@@ -12,7 +12,7 @@ const movies = {
 const genreList = Object.keys(movies)
 console.log(genreList)
 
-export default function insertsGenres() {
+export function insertsGenres() {
   for (let i = 0; i < genreList.length; i++) {
     // Creates a new option element for our home genre dropdown box
     const homeOptionElement = document.createElement('option')
